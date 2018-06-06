e602b1e84c064c79e2f3cf17b96d274d6577978b
Sebastian Podda <sebastianpodda@unica.it>
2018-06-06 12:05:52 +0200

    Merge STEFANO into MASTER

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 const AccessManager = require('../engine/access-manager');
 const ErrorHandler = require('../engine/error-handler');
+const AnalyticsManager = require('../engine/analytics-manager');
 
 module.exports = function (app, passport) {
 
@@ -7,22 +8,19 @@ module.exports = function (app, passport) {
     let indexPath = "/";
     let amPath = indexPath + "users/";
 
+    /* AUTH */
     const pauth = passport.authenticate.bind(passport);
     const sess = {session: false};
 
-    app.get('/users/getEmailFromUsername/:usern', accessRoute.getEmailFromUsername);
-    app.post('/login', passport.authenticate('basic', { session : false }), function (req, res, next) {
-        res.json("Logged In");
-    });
-    app.get('/login', accessRoute.login);
-    app.get('/fbfancount',facebookRoute.fbFanCount);
-    app.get('/', indexRoute.index);
-    app.use(indexRoute.fun404);
     /****************** ACCESS MANAGER ********************/
 
     app.post('/login', AccessManager.basicLogin);
     app.get('/ciao', pauth('jwt', sess), function (req, res, next) { res.send('ciao'); });
     app.get(amPath + 'getUserFromUsername/:usern', AccessManager.getUserFromUsername);
+
+    /****************** FACEBOOK MANAGER ********************/
+
+    app.get('/fbfancount', AnalyticsManager.fbFanCount);
 
     /****************** ERROR HANDLER ********************/
 
