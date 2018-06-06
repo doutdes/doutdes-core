60a11d1aaf6220c85b7780c19748b132351e1c99
Sebastian Podda <sebastianpodda@unica.it>
2018-06-06 11:59:29 +0200

    Merge MASTER and FEDERICO

--- config/routes.js
+++ config/routes.js
@@ -11,17 +11,10 @@ module.exports = function (app, passport) {
     const sess = {session: false};
 
     /****************** ACCESS MANAGER ********************/
-    app.get('/users/getEmailFromUsername/:usern', accessRoute.getEmailFromUsername);
 
     app.post('/login', AccessManager.basicLogin);
-
-    app.get('/ciao', pauth('jwt', sess), function (req, res, next) {
-        res.send('ciao');
-    });
-
+    app.get('/ciao', pauth('jwt', sess), function (req, res, next) { res.send('ciao'); });
     app.get(amPath + 'getUserFromUsername/:usern', AccessManager.getUserFromUsername);
-    app.post('/login', passport.authenticate('basic', { session : false }), function (req, res, next) { res.json("Logged In"); });
-
 
     /****************** ERROR HANDLER ********************/
 
