4f99728f91ab8b8e73235d2a4bdf1657647c198f
Ibbus93 <ibbus93@gmail.com>
2018-06-07 10:54:10 +0200

    Working on api keys

--- models/userkeys-model.js
+++ models/userkeys-model.js
@@ -1,26 +1,26 @@
 'use strict';
 
 module.exports = (sequelize, DataType) => {
-  var User_keys = sequelize.define('user_keys', {
-    user_id: {
-      type: DataType.INTEGER(5),
-      primaryKey: true,
-    },
-    service: {
-      type: DataType.INTEGER(3),
-      primaryKey: true
-    },
-    api_key: DataType.STRING(100)
-  }, {
-    freezeTableName: true,
-    timestamps: false,
-    tableName: 'user_keys'
-  });
+    let User_keys = sequelize.define('user_keys', {
+        user_id: {
+            type: DataType.INTEGER(5),
+            primaryKey: true,
+        },
+        service: {
+            type: DataType.INTEGER(3),
+            primaryKey: true
+        },
+        api_key: DataType.STRING(100)
+    }, {
+        freezeTableName: true,
+        timestamps: false,
+        tableName: 'user_keys'
+    });
 
-  User_keys.associate = function (models) {
-    User_keys.hasMany(models.Users, {foreignKey: 'id', sourceKey: models.Users.id})
-  };
+    User_keys.associate = function (models) {
+        User_keys.hasMany(models.Users, {foreignKey: 'id', sourceKey: models.Users.id})
+    };
 
-  return User_keys;
+    return User_keys;
 };
 
