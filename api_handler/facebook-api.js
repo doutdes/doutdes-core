69b1cf423bafde14b8c67395fe0d9166eb500341
StefanoChessa <sr.chessa@tiscali.it>
2018-06-13 15:54:37 +0200

    added 1 facebook insight method

--- api_handler/facebook-api.js
+++ api_handler/facebook-api.js
@@ -3,19 +3,55 @@
  * */
 
 const Request = require('request-promise');
+const page_id = '1397642170275248';
+const access_token = 'EAAYgMsLsh6kBAHaIb2LuEnDBn4k2KIYvZCgTqqoUeVk8R97ZATKLVRFbPuWr2ppeXwsRsEKxtRdKaqsUogJjaRq3B81UMkVYy5IBAmZAOhUKDvYZBntWjnA865bz8vamvclZAgy3gE3Uv6X4NM5EOeLq38viSq4u4QC80CfTZBfwZDZD'
 
 exports.fbFanCount = function () {
 
-    const page_id = '1397642170275248';
-    const access_token = 'EAACEdEose0cBANDe7CnAB2GwjTUGtydy3c3tr8WjuBOul3DuD5NAZBIqKg41F5QpiU5bViDgAs1w5CJ7qm2WHfdAXGroWgMRfnIOqjWxu8DJA7b0u3ZCczBeZACivWCNwK2KzQbn81zpGaQayhEJhYfllTR3w9k2VLr3wMiYXWgPIhZC2JAWsjZAqsmi3oM9bSyrzJhNoRgZDZD'
     const options = {
-        method : 'GET',
-        uri : 'https://graph.facebook.com/' + page_id,
-        qs : {
-            access_token : access_token,
-            fields : 'fan_count'
+        method: 'GET',
+        uri: 'https://graph.facebook.com/' + page_id,
+        qs: {
+            access_token: access_token,
+            fields: 'fan_count'
         }
     };
     return Request(options);
 };
+
+exports.fbInsights = function () {
+    const options = {
+        method: 'GET',
+        uri: 'https://graph.facebook.com/' + page_id + '/insights/',
+        qs: {
+            access_token: access_token,
+            metric: 'page_engaged_users',
+            period: 'days_28'
+        }
+    };
+    return Request(options);
+};
+
+
+// const {FB, FacebookApiException} = require('fb');
+//
+// FB.options({
+//     version: 'v3.0',
+//     accessToken: 'EAACEdEose0cBAIZB3e0oYIXKONS9F1sVCXZBajegts94uwfd4S05KZCcNbbmOEhulReJMdI6ezOTJkLKXvp2RBZARvBaqF5pGD8mCuXeW9nB7Rk3EKt70fHAKllGbcHx1A234NhhIZCvl1xHSpbffvwzeWQ7Jo0HMA0RJE0KXHJHcw7qt3wbEtaBxCA0Q1BHHaH3viMLDtQZDZD'
+// });
+// FB.extend({appID: '1397642170275248'});
+//
+// exports.fbFanCount = () => {
+//     return FB.api('4', {fields: ['id', 'name']}, function (res) {
+//         if (!res || res.error) {
+//             console.log(!res ? 'error occurred' : res.error);
+//             return;
+//         }
+//         console.log(res.id);
+//         console.log(res.name);
+//     });
+// };
+
+
+
 
--- engine/analytics-manager.js
+++ engine/analytics-manager.js
@@ -1,12 +1,40 @@
 'use strict';
 
-const FacebookApi = require ('../api_handler/facebook-api');
+const FacebookApi = require('../api_handler/facebook-api');
 
-exports.fbFanCount = function (req,res,next){
+//const {FB, FacebookApiException} = require('fb');
+
+exports.fbFanCount = function (req, res, next) {
 
     FacebookApi.fbFanCount()
-        .then(fan_count =>{
-            res.json(fan_count);})
+        .then(fan_count => {
+            res.json(fan_count);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+
+    // FB.options({
+    //     version: 'v2.6',
+    //     accessToken: 'EAAYgMsLsh6kBAK0kapfMxZBgMqtnzVuRO7XAOE42ZBgpF8LEZAjB56yYXZAWBu3CawvtZBWdY6Q9KY9Wim7QYzU6l4mikV5k7bAhUBm2YWuZBRRfIcorg8J3291o7kwj9Rm3UkvVRZCp1ZCTgmZAlRGZAFVONyfaBbVsS3SJE4bM8r3QZDZD'
+    // });
+    // //FB.extend({appID: '1724252250998697'});
+    //
+    // FB.api('1397642170275248' /*'1525666877712653'*/ , {insights : ['page_content_activity_by_action_type_unique']} , {fields: ['id', 'name', 'fan_count']} , function (resp) {
+    //     if (!resp || resp.error) {
+    //         console.log(!resp ? 'error occurred' : resp.error);
+    //         return;
+    //     }
+    //     res.send({id:resp.id, name: resp.name, fan: resp.fan_count});
+    // });
+};
+
+exports.fbInsights = function (req, res, next) {
+
+    FacebookApi.fbInsights()
+        .then(fbInsights => {
+            res.json(fbInsights);
+        })
         .catch(err => {
             res.json(err);
         });
