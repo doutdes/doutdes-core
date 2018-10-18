87f49a21dddea62016049846ff5faf5522d13881
Ibbus93 <ibbus93@gmail.com>
2018-10-18 12:44:42 +0200

    Init configuration apidoc

--- engine/access-manager.js
+++ engine/access-manager.js
@@ -8,28 +8,6 @@ const jwt = require('jsonwebtoken');
 
 const HttpStatus = require('http-status-codes');
 
-exports.roleAuthorization = function(roles){
-
-    return function(req, res, next){
-
-        let user = req.user;
-
-        Users.findById(user.id)
-            .then(userFound => {
-                if(roles.indexOf(userFound.user_type) > -1){
-                    return next();
-                }
-
-                res.status(401).json({error: 'You are not authorized to view this content'});
-                return next('Unauthorized');
-            })
-            .catch(err => {
-                res.status(422).json({error: 'No user found.'});
-                return next(err);
-            });
-    }
-};
-
 exports.createUser = function (req, res, next) {
     const Op = Model.Sequelize.Op;
     const user = req.body;
@@ -91,6 +69,68 @@ exports.createUser = function (req, res, next) {
     ;
 };
 
+/**
+ * @api {get} /users/getFromId/ Get From ID
+ * @apiName GetFromId
+ * @apiGroup User
+ * @apiPermission all
+ *
+ * @apiHeader {String} Authorization Json Web Token retrieved from login request.
+ *
+ * @apiHeaderExample {json} Header-Example:
+ *     {
+ *       "Authorization": "Bearer YW55X25hbWUiOm51bGwsInZhdF9udW1iZXIi"
+ *     }
+ *
+ *
+ * @apiSuccess {Number} id Identifier of the user.
+ * @apiSuccess {String} username Username of the user.
+ * @apiSuccess {String} email Email of the user.
+ * @apiSuccess {String} company_name Name of the company of the user.
+ * @apiSuccess {String} vat_number Vat Number of the company of the user.
+ * @apiSuccess {String} first_name First name of the user.
+ * @apiSuccess {String} last_name Last name of the user.
+ * @apiSuccess {String} birth_place Birth place of the user.
+ * @apiSuccess {Date} birth_date Birth date of the user.
+ * @apiSuccess {String} fiscal_code Fiscal code of the user.
+ * @apiSuccess {String} address Address of residence of the user.
+ * @apiSuccess {String} province Province of residence of the user.
+ * @apiSuccess {String} city City of residence of the user.
+ * @apiSuccess {String} zip Zip code associated to the city of residence of the user.
+ * @apiSuccess {String} password Password needed by the user to login to the platform.
+ * @apiSuccess {String} user_type Type of the user into the platform.
+ * @apiSuccess {String} checksum Field for verify the fairness of the other fields.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *          "id": 2,
+ *          "username": "admin",
+ *          "email": "admin",
+ *          "company_name": "myCompany",
+ *          "vat_number": 123456789456,
+ *          "first_name": "Michael",
+ *          "last_name": "Bohn",
+ *          "birth_place": "Land of Admins",
+ *          "birth_date": "1990-06-06",
+ *          "fiscal_code": "aaaddd93e92b292u",
+ *          "address": "Admin street, 23",
+ *          "province": "ad",
+ *          "city": "administration",
+ *          "zip": "01923",
+ *          "password": "a_password",
+ *          "user_type": "his_user_type",
+ *          "checksum": "a_nice_checksum"
+ *     }
+ *
+ * @apiError InternalServerError Error retrieving the user informations.
+ *
+ * @apiError Unauthorized The user is not authorized to do the request.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 401 Unauthorized
+ *          Unauthorized
+ */
 exports.getUserById = function (req, res, next) {
     Model.Users.findById(req.user.id)
         .then(user => {
@@ -98,7 +138,7 @@ exports.getUserById = function (req, res, next) {
         })
         .catch(err => {
             return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
-                error: err
+                error: 'Cannot GET the informations about the user.'
             });
         })
 };
@@ -177,4 +217,26 @@ exports.basicLogin = function (req, res, next) {
             return res.status(HttpStatus.OK).json({user, token});
         }
     })(req, res, next);
+};
+
+exports.roleAuthorization = function(roles){
+
+    return function(req, res, next){
+
+        let user = req.user;
+
+        Users.findById(user.id)
+            .then(userFound => {
+                if(roles.indexOf(userFound.user_type) > -1){
+                    return next();
+                }
+
+                res.status(401).json({error: 'You are not authorized to view this content'});
+                return next('Unauthorized');
+            })
+            .catch(err => {
+                res.status(422).json({error: 'No user found.'});
+                return next(err);
+            });
+    }
 };
