d8c448721d47b81ef737ccd4216e1cb42c5d5aaf
Stefano Chessa <sr.chessa@tiscali.it>
2018-10-24 10:39:38 +0200

    minor

--- engine/analytics/google-manager.js
+++ engine/analytics/google-manager.js
@@ -15,7 +15,6 @@ exports.ga_getLastYearSessions = async function (req, res, next) {
             user_id: req.user.id
         }
     }).then(key => {
-        //console.log("manager: " + key.client_email);
         GoogleApi.getLastYearSessions(key.client_email, key.private_key)
             .then(value => {
                 if (value.length === 0) {
@@ -27,20 +26,19 @@ exports.ga_getLastYearSessions = async function (req, res, next) {
                 console.log(err);
                 return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
                     error: true,
-                    message: 'Cannot get informations'
+                    message: 'Cannot retrieve data.'
                 })
             })
     }).catch(err => {
-        console.log(err);
         if (err.statusCode === 400) {
             return res.status(HttpStatus.BAD_REQUEST).send({
                 name: 'Google Analytics Bad Request',
-                message: 'Invalid OAuth access token.'
+                message: 'Invalid access token.'
             });
         }
         return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
             name: 'Google Analytics Internal Server Error',
-            message: 'There is a problem with Facebook servers'
+            message: 'There is a problem with an external source.'
         });
     });
 };
@@ -81,13 +79,69 @@ exports.ga_getPageViews = async function (req, res, next) {
 };
 
 exports.ga_getMostPagesViews = async function (req, res, next) {
-    let result = await GoogleApi.getMostPagesVisited();
-
-    return res.status(HttpStatus.OK).send(result);
+    GaToken.findOne({
+        where: {
+            user_id: req.user.id
+        }
+    }).then(key => {
+        GoogleApi.getMostPagesVisited(key.client_email, key.private_key)
+            .then(value => {
+                if (value.length === 0) {
+                    return res.status(HttpStatus.NO_CONTENT).send({});
+                } else
+                    return res.status(HttpStatus.OK).send(value);
+            })
+            .catch(err => {
+                console.log(err);
+                return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+                    error: true,
+                    message: 'Cannot retrieve data.'
+                })
+            })
+    }).catch(err => {
+        if (err.statusCode === 400) {
+            return res.status(HttpStatus.BAD_REQUEST).send({
+                name: 'Google Analytics Bad Request',
+                message: 'Invalid access token.'
+            });
+        }
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+            name: 'Google Analytics Internal Server Error',
+            message: 'There is a problem with an external source.'
+        });
+    });
 };
 
 exports.ga_getSources = async function (req, res, next) {
-    let result = await GoogleApi.getSources();
-
-    return res.status(HttpStatus.OK).send(result);
+    GaToken.findOne({
+        where: {
+            user_id: req.user.id
+        }
+    }).then(key => {
+        GoogleApi.getSources(key.client_email, key.private_key)
+            .then(value => {
+                if (value.length === 0) {
+                    return res.status(HttpStatus.NO_CONTENT).send({});
+                } else
+                    return res.status(HttpStatus.OK).send(value);
+            })
+            .catch(err => {
+                console.log(err);
+                return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+                    error: true,
+                    message: 'Cannot retrieve data.'
+                })
+            })
+    }).catch(err => {
+        if (err.statusCode === 400) {
+            return res.status(HttpStatus.BAD_REQUEST).send({
+                name: 'Google Analytics Bad Request',
+                message: 'Invalid access token.'
+            });
+        }
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+            name: 'Google Analytics Internal Server Error',
+            message: 'There is a problem with an external source.'
+        });
+    });
 };
