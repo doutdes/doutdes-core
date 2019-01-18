7b6313f2746ef295bd1250ca727a399f0e7e60a3
Ibbus93 <ibbus93@gmail.com>
2019-01-18 15:35:19 +0100

    Setting ENV support

--- config/passport/google.js
+++ config/passport/google.js
@@ -1,17 +1,15 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-const config = require('../config').production;
+const config = require('../../app').config;
 
-const clientID      = config.ga_client_id;
-const clientSecret  = config.ga_client_secret;
-const callbackURL   = 'http://localhost:443/ga/login/success';
+const clientID      = config['ga_client_id'];
+const clientSecret  = config['ga_client_secret'];
+const callbackURL   = config['site_URL'] + 'ga/login/success';
 
 module.exports = new GoogleStrategy({
     clientID: clientID,
     clientSecret: clientSecret,
     callbackURL: callbackURL
 }, (req, refreshToken, accessToken, profile, done) => {
-    console.log(accessToken);
-    console.log(refreshToken);
-    done(null, {accessToken: accessToken, refreshToken: refreshToken});
+    return done(null, {accessToken: accessToken, refreshToken: refreshToken});
 });
