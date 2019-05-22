dc2bf29b8c75d5f984eb857f72e34da9c7d02955
Ibbus93 <ibbus93@gmail.com>
2019-05-22 15:50:30 +0200

    Changed basic login response

--- engine/access-manager.js
+++ engine/access-manager.js
@@ -380,7 +380,8 @@ exports.basicLogin = function (req, res, next) {
                     'username': user.username,
                     'email': user.email,
                     'first_name': user.first_name,
-                    'last_name': user.last_name
+                    'last_name': user.last_name,
+                    'user_type': user.user_type
                 },
                 'token': token
             });
