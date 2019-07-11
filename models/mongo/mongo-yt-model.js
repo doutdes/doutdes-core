2931161b5196ba423655b25e4758836f90d96dde
Stefano Chessa <sr.chessa@tiscali.it>
2019-07-11 11:29:42 +0200

    added yt mongo model

---
+++ models/mongo/mongo-yt-model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+module.exports = mongoose.model('YtData', new mongoose.Schema({
+    userid: Number,
+    channel_id: String,
+    metric: String,
+    dimensions: String,
+    start_date: String,
+    end_date: String,
+    data: Array
+}, {
+    versionKey: false
+}));
